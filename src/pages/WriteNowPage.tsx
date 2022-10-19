7a35c15c7b0b6f1746b9379394c3375717ae7831
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-10-19 09:04:44 -0400

    atualização do projeto

--- src/pages/WriteNowPage.tsx
+++ src/pages/WriteNowPage.tsx
@@ -1,4 +1,5 @@
 import { Button, DateTimePicker, Nav, RichInput } from 'components';
+import { IWriteNowForm } from 'interfaces';
 import { FormProvider, useForm } from 'react-hook-form';
 import { WriteNowResolver } from 'validations';
 
@@ -24,21 +25,34 @@ export function WriteNowPage() {
             <label htmlFor="destinationName">Nome completo</label>
             <input {...register('destinationName')} type="text" />
 
-            {errors?.destionationName?.message && ( 
-            <p className="error-text">{errors?.destionationName?.message}</p>
+            {errors?.destinationName?.message && ( 
+            <p className="error-text">{errors?.destinationName?.message}</p>
             )}
 
+
             <label htmlFor="destinationAddress">E-mail</label>
-            <input {...register('destinationAddress')} type="text" />
+            <input {...register('destinationAddress')} type="email" />
+            {errors?.destinationAddress?.message && ( 
+            <p className="error-text">{errors?.destinationAddress?.message}</p>
+            )}
 
             <label htmlFor="dueData">Data</label>
             <DateTimePicker name="dueDate"/>
+            {errors?.dueDate?.message && ( 
+            <p className="error-text">{errors?.dueDate?.message}</p>
+            )}
 
             <label htmlFor="subject">Assunto</label>
             <input {...register('subject')} type="text" />
+            {errors?.subject?.message && ( 
+            <p className="error-text">{errors?.subject?.message}</p>
+            )}
 
             <label htmlFor="body">Mensagem</label>
             <RichInput name="body"/>
+            {errors?.body?.message && ( 
+            <p className="error-text">{errors?.body?.message}</p>
+            )}
 
             <Button type="submit" variant="primary">
               Enviar
